456944a231d5f9786788f91d79ee2eeb125c8735
Daniel Fredriksson <daniel.ls.fredriksson@gmail.com>
2019-10-24 14:09:50 +0200

    Code in GunSystem cleaned

--- Sail/src/Sail/entities/systems/Gameplay/GunSystem.cpp
+++ Sail/src/Sail/entities/systems/Gameplay/GunSystem.cpp
@@ -29,101 +29,124 @@ void GunSystem::update(float dt) {
 	for (auto& e : entities) {
 		GunComponent* gun = e->getComponent<GunComponent>();
 
-		// If gun is firing
+		// Gun is firing
 		if (gun->firing) {
 
-			// If gun is not overloaded
+			// Gun is not overloaded
 			if (gun->gunOverloadTimer <= 0) {
 
 				// SHOOT
 				if (gun->projectileSpawnTimer <= 0.f) {
-					gun->projectileSpawnTimer = gun->m_projectileSpawnCooldown;
-
-					// Stays here, not in receiver since this is neither per-frame or per-event.
-					// it is an event with a duration, something which needs its own definition and space
-					// if we decided to implement more of the same type. Until then it should be here.
-					// ( Same logic for the sounds being played later on in this update function ) 
-					EntityFactory::CreateProjectile(gun->position, gun->direction * gun->projectileSpeed, true);
-
-					// If this is the first shot in this "burst" of projectiles...
-					if (!gun->firingContinuously) {
-						// ...let the network know.
-						e->getComponent<NetworkSenderComponent>()->addDataType(
-							Netcode::MessageType::SHOOT_START
-						);
-
-						// Audio
-						gun->state = GunState::STARTING;
-					}
-					else {
-						gun->state = GunState::LOOPING;
-					}
-
-					gun->firingContinuously = true;
-					m_gameDataTracker->logWeaponFired();
+					fireGun(e, gun);
 				}
-				// DO NOT SHOOT
+				// DO NOT SHOOT (Cooldown between shots)
 				else {
 					gun->firingContinuously = false;
 				}
 
 				// Overload the gun if necessary
 				if ((gun->gunOverloadvalue += dt) > gun->gunOverloadThreshold) {
-					gun->gunOverloadTimer = gun->m_gunOverloadCooldown;
-					gun->gunOverloadvalue = 0;
-
-					// Let network know we stopped shooting (cus of cooldown).
-					std::cout << gun->firing << " | ";
-					std::cout << gun->firingContinuously << "\n";
-					e->getComponent<NetworkSenderComponent>()->addDataType(
-						Netcode::MessageType::SHOOT_END
-					);
-					gun->state = GunState::ENDING;
-
-					gun->firingContinuously = false;
+					overloadGun(e, gun);
 				}
 			}
 		}
+		// Gun is not firing
 		else {
 
+			// Reduce the overload value
 			if (gun->gunOverloadvalue > 0) {
 				gun->gunOverloadvalue -= dt;
 			}
 
-			// If we were firing last frame but now we are not...
+			// Gun stopped firing
 			if (gun->firingContinuously) {
-				// ...let network know we stopped shooting.
-				e->getComponent<NetworkSenderComponent>()->addDataType(
-					Netcode::MessageType::SHOOT_END
-				);
-				gun->state = GunState::ENDING;
+				// Set gun state to END
+				setGunStateEND(e, gun);
 			}
 
 			gun->firingContinuously = false;
 		}
 
-		// Play sounds depending on which state the gun is in.
-		if (gun->state == GunState::STARTING) {
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_START].isPlaying = true;
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_START].playOnce = true;
-		}
-		else if (gun->state == GunState::LOOPING) {
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_START].isPlaying = false;
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_LOOP].isPlaying = true;
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_LOOP].playOnce = true;
-		}
-		else if (gun->state == GunState::ENDING) {
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_LOOP].isPlaying = false;
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_END].isPlaying = true;
-			e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_END].playOnce = true;
-			
-			gun->state = GunState::STANDBY;
-		}
-		else {	/* gun->state == GunState::STANDBY */
-
-		}
+		// Play shooting sounds based on the GunState
+		playShootingSounds(e);
 
 		gun->gunOverloadTimer -= dt;
 		gun->projectileSpawnTimer -= dt;
+	}
+}
+
+
+void GunSystem::fireGun(Entity* e, GunComponent* gun) {
+	gun->projectileSpawnTimer = gun->m_projectileSpawnCooldown;
+
+	// Stays here, not in receiver since this is neither per-frame or per-event.
+	// it is an event with a duration, something which needs its own definition and space
+	// if we decided to implement more of the same type. Until then it should be here.
+	// ( Same logic for the sounds being played later on in this update function ) 
+	EntityFactory::CreateProjectile(gun->position, gun->direction * gun->projectileSpeed, true);
+
+	// If this is the first shot in this "burst" of projectiles...
+	if (!gun->firingContinuously) {
+		setGunStateSTART(e, gun);
+	}
+	else {
+		setGunStateLOOP(e, gun);
+	}
+
+	gun->firingContinuously = true;
+	m_gameDataTracker->logWeaponFired();
+}
+
+void GunSystem::overloadGun(Entity* e, GunComponent* gun) {
+	gun->gunOverloadTimer = gun->m_gunOverloadCooldown;
+	gun->gunOverloadvalue = 0;
+
+	setGunStateEND(e, gun);
+
+	gun->firingContinuously = false;
+}
+
+void GunSystem::setGunStateSTART(Entity* e, GunComponent* gun) {
+	e->getComponent<NetworkSenderComponent>()->addDataType(
+		Netcode::MessageType::SHOOT_START
+	);
+
+	gun->state = GunState::STARTING;
+}
+
+void GunSystem::setGunStateLOOP(Entity* e, GunComponent* gun) {
+	// Network automatically handles transition from starting-->looping
+	gun->state = GunState::LOOPING;
+}
+
+void GunSystem::setGunStateEND(Entity* e, GunComponent* gun) {
+	e->getComponent<NetworkSenderComponent>()->addDataType(
+		Netcode::MessageType::SHOOT_END
+	);
+	gun->state = GunState::ENDING;
+}
+
+void GunSystem::playShootingSounds(Entity* e) {
+	GunComponent* gun = e->getComponent<GunComponent>();
+
+	// Play sounds depending on which state the gun is in.
+	if (gun->state == GunState::STARTING) {
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_START].isPlaying = true;
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_START].playOnce = true;
+	}
+	else if (gun->state == GunState::LOOPING) {
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_START].isPlaying = false;
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_LOOP].isPlaying = true;
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_LOOP].playOnce = true;
+	}
+	else if (gun->state == GunState::ENDING) {
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_LOOP].isPlaying = false;
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_END].isPlaying = true;
+		e->getComponent<AudioComponent>()->m_sounds[Audio::SoundType::SHOOT_END].playOnce = true;
+			
+		gun->state = GunState::STANDBY;
+	}
+	else {	/* gun->state == GunState::STANDBY */
+
 	}
 }
